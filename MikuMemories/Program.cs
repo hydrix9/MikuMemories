2059774770b85f9620e6140284fd9403f5e80f56
chad <chadlbourke@yahoo.com>
2023-04-20 22:42:42 -1000

    initial commit

---
+++ MikuMemories/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MikuMemories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Config(); //create instance
+            new Mongo(); //create instance
+            new LlmApi(); //create instance
+
+
+            var request = new LLmApiRequest(LlmInputParams.defaultParams);
+            request.prompt = "hello";
+            request.@params.max_new_tokens = 5;
+
+            Console.WriteLine(request.AsString());
+
+            LlmApi.QueueRequest(request);
+
+            while(true)
+            {
+                LlmApi.instance.TryProcessQueue();
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ MikuMemories/RestApi.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMemories
+{
+    public class RestApi
+    {
+        public static string PostRequest(string url, string json)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Method = "POST";
+
+            string result = "";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                //Debug.Write(json);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            try
+            {
+                using (var response = httpWebRequest.GetResponse() as HttpWebResponse)
+                {
+                    if (httpWebRequest.HaveResponse && response != null)
+                    {
+                        using (var reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            result = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                if (e.Response != null)
+                {
+                    using (var errorResponse = (HttpWebResponse)e.Response)
+                    {
+                        using (var reader = new StreamReader(errorResponse.GetResponseStream()))
+                        {
+                            string error = reader.ReadToEnd();
+                            result = error;
+                        }
+                    }
+
+                }
+            }
+
+            return result;
+
+        }
+
+    }
+}
