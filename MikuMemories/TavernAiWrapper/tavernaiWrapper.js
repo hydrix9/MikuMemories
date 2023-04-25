fbe967b4b61522cadffc74f205052118577e12a4
= <chadlbourke@yahoo.com>
2023-04-24 17:49:04 -1000

    adding character cards and graceful shutdown

--- MikuMemories/TavernAiWrapper/tavernaiWrapper.js
+++ MikuMemories/TavernAiWrapper/tavernaiWrapper.js
@@ -1,29 +1,48 @@
-const fs = require("fs");
-const PNG = require("pngjs").PNG;
+const fs = require('fs');
+const jimp = require('jimp');
+const extract = require('png-chunks-extract');
+const encode = require('png-chunks-encode');
+const PNGtext = require('png-chunk-text');
+const ExifReader = require('exifreader');
+const webp = require('webp-converter');
+const path = require('path');
 
-function loadJsonFromPng(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(filePath)
-      .pipe(new PNG())
-      .on("parsed", function () {
-        let jsonData = "";
-        for (let y = 0; y < this.height; y++) {
-          for (let x = 0; x < this.width; x++) {
-            let idx = (this.width * y + x) << 2;
-            let r = this.data[idx];
-            if (r !== 0) {
-              jsonData += String.fromCharCode(r);
-            }
-          }
+async function loadJsonFromPng(img_url, input_format) {
+    let format;
+    if (input_format === undefined) {
+        if (img_url.indexOf('.webp') !== -1) {
+            format = 'webp';
+        } else {
+            format = 'png';
         }
-        resolve(jsonData);
-      })
-      .on("error", function (error) {
-        reject(error);
-      });
-  });
+    } else {
+        format = input_format;
+    }
+
+    switch (format) {
+        case 'webp':
+            const exif_data = await ExifReader.load(fs.readFileSync(img_url));
+            const char_data = exif_data['UserComment']['description'];
+            if (char_data === 'Undefined' && exif_data['UserComment'].value && exif_data['UserComment'].value.length === 1) {
+                return exif_data['UserComment'].value[0];
+            }
+            return char_data;
+        case 'png':
+            const buffer = fs.readFileSync(img_url);
+            const chunks = extract(buffer);
+
+            const textChunks = chunks.filter(function (chunk) {
+                return chunk.name === 'tEXt';
+            }).map(function (chunk) {
+                return PNGtext.decode(chunk.data);
+            });
+            var base64DecodedData = Buffer.from(textChunks[0].text, 'base64').toString('utf8');
+            return base64DecodedData;
+        default:
+            break;
+    }
 }
 
 module.exports = {
-  loadJsonFromPng: loadJsonFromPng,
+    loadJsonFromPng,
 };
